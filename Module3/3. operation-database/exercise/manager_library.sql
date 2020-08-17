1e45b1dfefb24a4e075af3e85c00b6e76621c99e
quangnguyendn96 <65935936+quangnguyendn96@users.noreply.github.com>
2020-08-17 23:42:41 +0700

    t2

--- Module3/3. operation-database/exercise/manager_library.sql
+++ Module3/3. operation-database/exercise/manager_library.sql
@@ -8,11 +8,11 @@ create table address_student(
 	country varchar(50)
 );
 insert into address_student(house_number,city,country) values 
-('k45/21 Phan Tu','Da Nang','Viet Nam'),
-('25 Le Dinh Ly','Da Nang','Viet Nam'),
-('45 Ngu Hanh Son','Da Nang','Viet Nam'),
-('21 Phan Thanh','Da Nang','Viet Nam'),
-('k45 Nguyen Van Thoai','Da Nang','Viet Nam');
+	('k45/21 Phan Tu','Da Nang','Viet Nam'),
+	('25 Le Dinh Ly','Da Nang','Viet Nam'),
+	('45 Ngu Hanh Son','Da Nang','Viet Nam'),
+	('21 Phan Thanh','Da Nang','Viet Nam'),
+	('k45 Nguyen Van Thoai','Da Nang','Viet Nam');
 
 create table student(
 	id_student varchar(15) primary key,
@@ -33,22 +33,22 @@ create table student_card(
 	foreign key (id_student) references student(id_student)
 );
 insert into student_card values 
-('100'),
-('220'),
-('214'),
-('614'),
-('198');
+	('100'),
+	('220'),
+	('214'),
+	('614'),
+	('198');
 
 create table category(
 	cate_number varchar(50) primary key,
 	type_book varchar(50) not null
 );
 insert into category(cate_number,type_book) values 
-('01111','comedy'),
-('02222','science fiction'),
-('03333','love'),
-('04444','sex'),
-('05555','comedy');
+	('01111','comedy'),
+	('02222','science fiction'),
+	('03333','love'),
+	('04444','sex'),
+	('05555','comedy');
 
 create table book(
 	name_book varchar(50) not null,
@@ -56,14 +56,17 @@ create table book(
     year_release varchar(50) not null,
     price double,
 	cate_number varchar(50) primary key,
-    foreign key(cate_number) references category(cate_number)
+    foreign key(cate_number) references category(cate_number),
+    type_book varchar(50) not null unique 
 );
+alter table category
+	add foreign key(type_book) references book(type_book);
 
 insert into book(name_book,publisher,year_release,price,cate_number) values
-('how to get married','ngon tinh','2020','1.000000','01111'),
-('how to have girlfriend immeidately','ngon tinh','2.000000','2010','02222'),
-('how to become goodboy','htvn','2019','3.000000','03333'),
-('making approaches to a girls secert','thanh nien','2022','4.000000','05555');
+	('how to get married','ngon tinh','2020','1.000000','01111'),
+	('how to have girlfriend immeidately','ngon tinh','2.000000','2010','02222'),
+	('how to become goodboy','htvn','2019','3.000000','03333'),
+	('making approaches to a girls secert','thanh nien','2022','4.000000','05555');
 
 create table borrow_order(
 	day_borrow date not null,
@@ -75,9 +78,9 @@ create table borrow_order(
 );
 
 insert into borrow_order(day_borrow,date_return,cate_number,id_student) values
-('2020-06-10','2020-12-10','02222','198'),
-('2020-02-25','2020-07-25','05555','198'),
-('2020-09-20','2021-01-20','02222','614');
+	('2020-06-10','2020-12-10','02222','198'),
+	('2020-02-25','2020-07-25','05555','198'),
+	('2020-09-20','2021-01-20','02222','614');
 
 -- are student borrowing book?? 
 select * from student_card as st
